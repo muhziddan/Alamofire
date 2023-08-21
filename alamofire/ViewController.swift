7e2595c88c7667722f67677a3f4cc53493ade052
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2023-08-21 08:43:45 +0700

    initial xcode project

---
+++ alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alamofire
+//
+//  Created by Muhammad Ziddan Hidayatullah on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
